890fbeef5b2faf249c633dcd88e2965e3319a180
Sam <sadanilov@edu.hse.ru>
2017-05-26 12:48:45 +0300

    danilov-web

---
+++ src/main/java/com/example/DatabaseConfig.java
@@ -0,0 +1,11 @@
+package com.example;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
