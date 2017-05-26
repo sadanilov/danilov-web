e9d68e8d5aeff563a726c6f16b2a5cd252a65d53
Sam <sadanilov@edu.hse.ru>
2017-05-26 12:27:03 +0300

    danilov-web

---
+++ src/main/java/com/example/DanilovApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanilovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanilovApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/Person.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/
+@Data
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String lastName;
+}
